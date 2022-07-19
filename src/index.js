f741c2ecad5d16744b360bd31673234ca929f423
diego <diegoyon@outlook.com>
2022-07-19 16:47:54 -0600

    Add refresh button functionality to get scores from the API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
-import './style.css';
+import "./style.css";
+// CREATE A NEW GAME
+// fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     name: "Squid Game",
+//   }),
+// })
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => console.log(data.result));
+
+// ADD A SCORE
+// fetch(
+//   "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jvP2RQFJf7ELeHvIFZYY/scores/",
+//   {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       user: "Diego",
+//       score: 98,
+//     }),
+//   }
+// )
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => console.log(data.result));
+
+// GET SCORES
+// fetch(
+//   "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jvP2RQFJf7ELeHvIFZYY/scores/"
+// )
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => console.log(data.result));
+
+const getScores = async () => {
+  const response = await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jvP2RQFJf7ELeHvIFZYY/scores/"
+  );
+  const JsonResponse = await response.json();
+  // console.log(JsonResponse.result);
+  updateScores(JsonResponse.result);
+  return JsonResponse.result;
+};
+
+const refreshButton = document.querySelector(".scores-header button");
+refreshButton.addEventListener("click", getScores);
+
+const updateScores = (scores) => {
+  const list = document.querySelector("ul");
+  list.innerHTML = "";
+  for (let i = 0; i < scores.length; i += 1) {
+    const li = document.createElement("li");
+    li.innerHTML = `${scores[i].user}: ${scores[i].score}`;
+    list.appendChild(li);
+  }
+};
