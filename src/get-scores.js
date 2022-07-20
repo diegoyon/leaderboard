77da5aafb45269995daa257cae6ebfabc4ce0ed6
diego <diegoyon@outlook.com>
2022-07-20 11:31:14 -0600

    Fix linters errors

--- src/get-scores.js
+++ src/get-scores.js
@@ -1,10 +1,10 @@
 const updateScores = (scores) => {
-  const list = document.querySelector("ul");
-  list.innerHTML = "<li><div>Username</div><div>Score</div></li>";
+  const list = document.querySelector('ul');
+  list.innerHTML = '<li><div>Username</div><div>Score</div></li>';
   for (let i = 0; i < scores.length; i += 1) {
-    const li = document.createElement("li");
-    const userName = document.createElement("div");
-    const userScore = document.createElement("div");
+    const li = document.createElement('li');
+    const userName = document.createElement('div');
+    const userScore = document.createElement('div');
     userName.innerHTML = scores[i].user;
     userScore.innerHTML = scores[i].score;
     li.appendChild(userName);
@@ -15,7 +15,7 @@ const updateScores = (scores) => {
 
 const getScores = async () => {
   const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IMbLUNoaRd8W6ClY4Px1/scores/"
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IMbLUNoaRd8W6ClY4Px1/scores/',
   );
   const JsonResponse = await response.json();
   updateScores(JsonResponse.result);
