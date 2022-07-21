584566b4cde36f5d07fd083aa1fac9d94dce3ed9
diego <diegoyon@outlook.com>
2022-07-21 09:46:29 -0600

    Add scroll to leaderboard

--- src/add-score.js
+++ src/add-score.js
@@ -1,24 +1,24 @@
 const addScore = async (userName, userScore) => {
   const response = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IMbLUNoaRd8W6ClY4Px1/scores/',
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IMbLUNoaRd8W6ClY4Px1/scores/",
     {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         user: userName,
         score: userScore,
       }),
-    },
+    }
   );
 
   const JsonResponse = await response.json();
 
-  const message = document.createElement('h5');
-  message.innerHTML = `${JsonResponse.result}. Click the refresh button to see changes.`;
+  const message = document.createElement("h5");
+  message.innerHTML = `${JsonResponse.result} Click the refresh button to see changes.`;
 
-  const body = document.querySelector('body');
+  const body = document.querySelector("body");
   body.appendChild(message);
 
   setTimeout(() => {
