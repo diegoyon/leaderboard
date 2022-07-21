e9e4480b9fc56447efee532e4b58d69cafa1a9c3
diego <diegoyon@outlook.com>
2022-07-21 10:28:46 -0600

    Add invalid input validation

--- src/add-score.js
+++ src/add-score.js
@@ -1,29 +1,41 @@
 const addScore = async (userName, userScore) => {
-  const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IMbLUNoaRd8W6ClY4Px1/scores/",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user: userName,
-        score: userScore,
-      }),
-    }
-  );
+  if (userName === "" || userScore === "") {
+    const message = document.createElement("h5");
+    message.innerHTML = `Invalid input.`;
 
-  const JsonResponse = await response.json();
+    const body = document.querySelector("body");
+    body.appendChild(message);
 
-  const message = document.createElement("h5");
-  message.innerHTML = `${JsonResponse.result} Click the refresh button to see changes.`;
+    setTimeout(() => {
+      message.remove();
+    }, 3000);
+  } else {
+    const response = await fetch(
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IMbLUNoaRd8W6ClY4Px1/scores/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: userName,
+          score: userScore,
+        }),
+      }
+    );
 
-  const body = document.querySelector("body");
-  body.appendChild(message);
+    const JsonResponse = await response.json();
 
-  setTimeout(() => {
-    message.remove();
-  }, 3000);
+    const message = document.createElement("h5");
+    message.innerHTML = `${JsonResponse.result} Click the refresh button to see changes.`;
+
+    const body = document.querySelector("body");
+    body.appendChild(message);
+
+    setTimeout(() => {
+      message.remove();
+    }, 3000);
+  }
 };
 
 export default addScore;
