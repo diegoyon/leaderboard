64be4ee5c2e8eb1594e13f04eafcfcd0a2e76b30
diego <diegoyon@outlook.com>
2022-07-22 16:50:22 -0600

    Add display flex to body and fix linters errors

--- src/create-game.js
+++ src/create-game.js
@@ -1,12 +1,11 @@
 // CREATE A NEW GAME
-fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/", {
-  method: "POST",
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+  method: 'POST',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
   body: JSON.stringify({
-    name: "Squid Game",
+    name: 'Squid Game',
   }),
-})
-  .then((res) => res.json())
-  .then((data) => console.log(data.result));
+}).then((res) => res.json());
+// .then((data) => console.log(data.result));
